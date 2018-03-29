b66f0bae2908d7f492240568629b868a9e082313
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-03-29 14:20:35 +0200

    Initial commit

---
+++ src/main/java/com/javadev/organizer/JavaDevOrganizerApplication.java
@@ -0,0 +1,12 @@
+package com.javadev.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevOrganizerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javadev/organizer/JavaDevOrganizerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javadev.organizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaDevOrganizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
