7a77586fdb44cc456181458b908ed4216b896191
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-04-11 23:59:05 +0200

    Cors config

--- src/main/java/com/javadev/organizer/config/SecurityConfig.java
+++ src/main/java/com/javadev/organizer/config/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{			
 		http
+			.cors()
+		.and()
 			.authorizeRequests()
 				.antMatchers("/student/**").hasAuthority("STUDENT")
 				.antMatchers("/lecturer/**").hasAnyAuthority("LECTURER","ADMIN")
