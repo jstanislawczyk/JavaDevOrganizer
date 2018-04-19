77420c557195908487231fda664beec655872092
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-04-19 10:22:06 +0200

    Role valid in admin controller

--- src/main/java/com/javadev/organizer/controllers/AdminController.java
+++ src/main/java/com/javadev/organizer/controllers/AdminController.java
@@ -3,24 +3,33 @@ package com.javadev.organizer.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.EnumUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.javadev.organizer.entities.Role;
 import com.javadev.organizer.entities.User;
 import com.javadev.organizer.repositories.UserRepository;
 
 @RestController
 public class AdminController {
 	
-	@Autowired
 	private UserRepository userRepository;
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public AdminController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 	
 	@GetMapping("/admin_control/find_all_users")
 	@PreAuthorize("hasAuthority('ADMIN')")
@@ -38,6 +47,19 @@ public class AdminController {
 	@PostMapping("/admin_control/create_user")
 	@PreAuthorize("hasAuthority('ADMIN')")
 	public void saveUser(@RequestBody User user) {
+		setupUser(user);
 		userRepository.save(user);
+	}
+	
+	private void setupUser(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		validRole(user);
+	}
+	
+	private void validRole(User user) {
+		
+		if(!EnumUtils.isValidEnum(Role.class, user.getRole())) {
+			user.setRole(Role.STUDENT.name());
+		}
 	}
 }
