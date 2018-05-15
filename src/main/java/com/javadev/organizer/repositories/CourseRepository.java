8472f1fa3382ff3fddb627d2c008f9c817c83145
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-05-15 18:17:53 +0200

    changes in repositories

--- src/main/java/com/javadev/organizer/repositories/CourseRepository.java
+++ src/main/java/com/javadev/organizer/repositories/CourseRepository.java
@@ -1,20 +1,9 @@
 package com.javadev.organizer.repositories;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.javadev.organizer.entities.Course;
 
 public interface CourseRepository extends CrudRepository<Course, Long> {
 	
-	@Query(value =   "SELECT id FROM course AS C "
-					+ "INNER JOIN course_users AS CU "
-					+ "ON C.id=CU.courses_id "
-					+ "WHERE users_id = :userId"
-			, nativeQuery = true)
-	List<BigDecimal> findCoursesIdsByUserId(@Param("userId") Long userId);
 }
--- src/main/java/com/javadev/organizer/repositories/UserRepository.java
+++ src/main/java/com/javadev/organizer/repositories/UserRepository.java
@@ -5,10 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.javadev.organizer.entities.Role;
 import com.javadev.organizer.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	List<User> findByRole(String role);
+	List<User> findByRole(Role role);
 	Optional<User> findByEmail(String email);
 	Long countByEmail(String email);
 }
