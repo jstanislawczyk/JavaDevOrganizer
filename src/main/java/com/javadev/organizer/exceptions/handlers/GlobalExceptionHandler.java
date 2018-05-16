7051d81a8719ca73fd28359c0a20450c0f34fb55
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-05-16 22:44:55 +0200

    changed exception logger info

--- src/main/java/com/javadev/organizer/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/javadev/organizer/exceptions/handlers/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.javadev.organizer.exceptions.handlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,28 +21,32 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public Error notFoundHandler(NotFoundException notFoundException) {
-		logger.info(notFoundException.getMessage());
+		logger.info("EXCEPTION | [email="+getCurrentUserEmail()+"]: "+notFoundException.getMessage());
 		return new Error(404, notFoundException.getMessage());
 	}
 	
 	@ExceptionHandler(NotUniqueException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public Error notUniqueHandler(NotUniqueException notUniqueException) {
-		logger.info(notUniqueException.getMessage());
+		logger.info("EXCEPTION | [email="+getCurrentUserEmail()+"]: "+notUniqueException.getMessage());
 		return new Error(409, notUniqueException.getMessage());
 	}
 	
 	@ExceptionHandler(NotSavedException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public Error notSavedHandler(NotSavedException notSavedException) {
-		logger.info(notSavedException.getMessage());
+		logger.info("EXCEPTION | [email="+getCurrentUserEmail()+"]: "+notSavedException.getMessage());
 		return new Error(409, notSavedException.getMessage());
 	}
 	
 	@ExceptionHandler(NotDeletedException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public Error notDeletedHandler(NotDeletedException notDeletedException) {
-		logger.info(notDeletedException.getMessage());
+		logger.info("EXCEPTION | [email="+getCurrentUserEmail()+"]: "+notDeletedException.getMessage());
 		return new Error(409, notDeletedException.getMessage());
+	}
+	
+	private String getCurrentUserEmail() {
+		return SecurityContextHolder.getContext().getAuthentication().getName();
 	}
 }
