6e1f993737bad5480f9268a626ddf42dfbae049f
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-05-16 16:47:29 +0200

    Basic logging for exceptions

--- src/main/java/com/javadev/organizer/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/javadev/organizer/exceptions/handlers/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.javadev.organizer.exceptions.handlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,27 +15,33 @@ import com.javadev.organizer.exceptions.NotUniqueException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public Error notFoundHandler(NotFoundException notFoundException) {
+		logger.info(notFoundException.getMessage());
 		return new Error(404, notFoundException.getMessage());
 	}
 	
 	@ExceptionHandler(NotUniqueException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public Error notUniqueHandler(NotUniqueException notUniqueException) {
+		logger.info(notUniqueException.getMessage());
 		return new Error(409, notUniqueException.getMessage());
 	}
 	
 	@ExceptionHandler(NotSavedException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public Error notSavedHandler(NotSavedException notSavedException) {
+		logger.info(notSavedException.getMessage());
 		return new Error(409, notSavedException.getMessage());
 	}
 	
 	@ExceptionHandler(NotDeletedException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public Error notDeletedHandler(NotDeletedException notDeletedException) {
+		logger.info(notDeletedException.getMessage());
 		return new Error(409, notDeletedException.getMessage());
 	}
 }
