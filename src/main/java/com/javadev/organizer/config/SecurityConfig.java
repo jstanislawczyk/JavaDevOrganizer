f2775cd45d20bf65ebfa33b5f77f6ef7cace7a93
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-05-16 23:55:15 +0200

    getCurrentLoggedInUserEmail method migrated to SecurityConfig

--- src/main/java/com/javadev/organizer/config/SecurityConfig.java
+++ src/main/java/com/javadev/organizer/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -55,5 +56,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
+	}
+	
+	public static String getCurrentLoggedInUserEmail() {
+		return SecurityContextHolder.getContext().getAuthentication().getName();
 	}
 }
