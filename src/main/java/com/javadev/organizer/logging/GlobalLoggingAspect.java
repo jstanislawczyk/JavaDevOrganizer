42bea751b30166b798fac2523a554c1f76ef0b9d
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-06-04 18:10:24 +0200

    Cleanup

--- src/main/java/com/javadev/organizer/logging/GlobalLoggingAspect.java
+++ src/main/java/com/javadev/organizer/logging/GlobalLoggingAspect.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.javadev.organizer.security.SecurityConfig;
+import com.javadev.organizer.config.SecurityConfig;
 
 @Aspect
 @Component
